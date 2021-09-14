8df4ab943c8d8bd2e25804a5c2e8f2666443bfff
Septiana Yoga <septianayoga30@gmail.com>
2021-09-14 14:34:33 +0700

    adding initial structure for the code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+      </div>
     </div>
   );
 }
