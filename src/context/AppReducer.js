23f48b3269474e0d44d95ea7aba89c6301360eb1
Septiana Yoga <septianayoga30@gmail.com>
2021-09-14 17:29:10 +0700

    adding functionality to the delete transaction

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,7 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
     default:
       return state;
   }
-}
+};
